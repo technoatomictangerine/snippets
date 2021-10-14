6c41e7c2cf2a76fbe728712476d0e937904eb59a
@scuroin <sumpilot@gmail.com>
2021-10-14 05:31:37 +0300

    Update task.lua

--- task.lua
+++ task.lua
@@ -1,95 +1,82 @@
-local timerSimple, next, assert, isfunction, pcall = timer.Simple, next, assert, isfunction, pcall
-local rate = 0
+local yield = coroutine.yield
+local create = coroutine.create
+local resume = coroutine.resume
+local timerSimple = timer.Simple
+local SysTime = SysTime
+local call = hook.Run
 
-local GetTable, RemoveID, Create, GetCurrent do
-	local tsks, rntsk, index = {}, false, 0
+local Create, GetTable do
+	local Run, co, running, start
+	local pcall, ErrorNoHalt = pcall, ErrorNoHalt
+	local index, list, names = 0, {}, {}
+	local rate = 1 / 300
+	local wait = 1 / 16
 
-	local function _unpack(tb, k, l)
-		local k, l = k or 1, l or #tb
-		if k <= l then
-			return tb[k], _unpack(tb, k + 1, l)
-		end
-	end
-
-	local function RunNextTask()
-		local k = next(tsks)
-
-		if k == nil then
-			rntsk = false
-			return
-		end
-
-		local data = tsks[k]
-
-		local s, err = pcall(data.func, data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8])
-		if s ~= true then
-			ErrorNoHalt('Failed task ', data.name, ' with error:\n\t', err, '\n')
-		end
-		tsks[k] = nil
-
-		timerSimple(rate, RunNextTask)
-	end
-
-	function GetTable()
-		return tasks
-	end
-
-	function Remove(name)
-		for k, data in next, tsks do
-			if data.name == name then
-				tsks[k] = nil
-			end
-		end
-	end
-
-	function RemoveID(id)
-		tsks[id] = nil
-	end
+	function GetTable() return list, index end
 
 	function Create(name, fn, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
-		assert(isfunction(fn) == true)
-		index = 1 + index
-		local dbg = debug.getinfo(3, 'S')
-		tsks[index] = {
-			arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8,
-			name = name,
-			func = fn,
-			src = dbg.short_src
-		}
-
-		if rntsk == false then
-			rntsk = true
-			timerSimple(rate, RunNextTask)
+		index = index + 1
+		names[index] = name
+		list[index] = function()
+			return pcall(fn, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
 		end
-
-		return id
+		if not running then
+			running = true
+			timerSimple(wait, Run)
+		end
+		return index
 	end
 
-	function GetCurrent()
-		return next(tsks)
+	function Run()
+		start = SysTime()
+		local success, breakruntime = resume(co)
+		if not success or breakruntime then
+			running = false
+		else
+			timerSimple(wait, Run)
+		end
 	end
+
+	co = create(function()
+		while true do
+			local fn, name = list[index], names[index]
+			index = index - 1
+			if fn ~= nil then
+				if SysTime() - start > rate then yield() end
+				local succ, err = fn()
+				if not succ then
+					ErrorNoHalt('Task failed: ', name, '\n\t', err, '\n')
+				end
+			end
+			if index == 0 then yield(true) end
+		end
+	end)
 end
 
 local NewThread, GetThreadTable do
+	local format = string.format
 	local timerCreate = timer.Create
 	local timerRemove = timer.Remove
-	local yield = coroutine.yield
 	local wait = coroutine.wait
-	local resume = coroutine.resume
-	local create = coroutine.create
 	local status = coroutine.status
-	local SysTime = SysTime
-	local mt = {counter = 0, __proto = 'threadobj'}
+	local start = 0
+	local mt = {counter = 0}
 	local pool = {}
 	mt.__index = mt
 
 	function mt:Remove()
-		timerRemove(self.name)
+		timerRemove(self.id)
+		call('ThreadDestroy', self.name)
 	end
 
 	function mt:Destroy()
 		self.Callback()
-		timerRemove(self.name)
+		timerRemove(self.id)
+		call('ThreadDestroy', self.name)
+	end
+
+	function mt:SetRemoveCondition(fn)
+		self.shouldrm = fn
 	end
 
 	function mt:yield(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
@@ -108,6 +95,10 @@ local NewThread, GetThreadTable do
 		end
 	end
 
+	function mt:limit(c)
+		if SysTime() - start > c then yield() end
+	end
+
 	function mt:status()
 		return status(self.thread)
 	end
@@ -115,7 +106,8 @@ local NewThread, GetThreadTable do
 	function mt:wrap(fn, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
 		local co = create(fn)
 		self.Callback = function()
-			if status(co) == 'dead' then self:Remove() return end
+			if status(co) == 'dead' or (self.shouldrm and self:shouldrm()) then self:Remove() return end
+			start = SysTime()
 			local succ, out1, out2, out3, out4, out5, out6, out7, out8 =
 				resume(co, self, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
 			if not succ then Error(out1) end
@@ -134,9 +126,11 @@ local NewThread, GetThreadTable do
 	end
 
 	function NewThread(name, rate, fn, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
-		name = 'ContinuesThreadObject::' .. name
-		local obj = setmetatable({name = name, start = SysTime()}, mt)
-		timerCreate(name, rate, 0, obj:wrap(fn, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8))
+		call('ThreadRunning', name)
+		local id = format('ThreadObject::%s', name)
+		local obj = setmetatable({name = name, id = id, start = SysTime()}, mt)
+		timerCreate(id,
+			rate, 0, obj:wrap(fn, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8))
 		return obj
 	end
 
@@ -146,10 +140,8 @@ local NewThread, GetThreadTable do
 end
 
 task = {
-	GetTable = GetTable,
-	RemoveID = RemoveID,
 	Create = Create,
-	GetCurrent = GetCurrent,
+	GetTable = GetTable,
 	NewThread = NewThread,
 	GetThreadTable = GetThreadTable
 }
